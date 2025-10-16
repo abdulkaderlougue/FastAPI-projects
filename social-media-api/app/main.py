5994f812d6963b06b091ac08831f696104421d21
abdulkaderlougue <engineerkaderit20@gmail.com>
2025-10-15 19:58:17 -0600

    integrated real postgress db using psycopg2, cleaned up unnecessary functions

--- social-media-api/app/main.py
+++ social-media-api/app/main.py
@@ -48,104 +48,6 @@ while True:
 
 api = FastAPI()
 
-def find_post(id):
-    posts = read_posts()
-
-    for post in posts:
-        if post['id'] == id:
-            return {"detail":post}
-
-def read_posts():
-    """
-    get the posts from the json db and return all the posts
-    """
-    try:
-        with open(JSON_DB, 'r') as f:
-            posts = json.load(f)
-    except FileNotFoundError:
-        print("File not found")
-        posts = []
-    except json.JSONDecodeError:
-        print(f"Warning: '{JSON_DB}' is empty or contains invalid JSON. Starting with an empty list.")
-        posts = []
-    
-    return posts
-
-def find_post_index(id):
-    """
-    Take id of a post and return the index of the post in the json database
-    return None if the post does not exist
-    """
-    posts = read_posts()
-    # print("==========",type(posts[0]))
-    for idx, post in enumerate(posts):
-        if post['id'] == id:
-            return idx
-    return None
-        
-def remove_post(id):
-    idx = find_post_index(id)
-    if not idx:
-        # if the index does not exist
-        return False
-    
-    try:
-        with open(JSON_DB, 'r') as f:
-            old_data = json.load(f)
-    except FileNotFoundError:
-        # file not found, start with an empty list
-        return False
-    except json.JSONDecodeError:
-        # file is empty or has invalid JSON
-        print(f"Warning: '{JSON_DB}' is empty or contains invalid JSON. Starting with an empty list.")
-        return False
-    
-    # delete the post
-    old_data.pop(idx)
-
-    # write back to the json db
-    with open(JSON_DB, 'w') as f:
-        json.dump(old_data, f, indent=3)
-
-    
-    return True
-
-def update_post_func(updated_post):
-
-    # remove the old post
-    is_remove = remove_post(updated_post['id'])
-    
-    if is_remove:
-        # add the updated post 
-        write_to_json_db(updated_post)
-        return True
-
-    return False
-
-
-def write_to_json_db(post_dict):
-    """
-    Add a post to the db
-    """
-    try:
-        with open(JSON_DB, 'r') as f:
-            old_data = json.load(f)
-    except FileNotFoundError:
-        # file not found, start with an empty list
-        old_data = []
-    except json.JSONDecodeError:
-        # file is empty or has invalid JSON
-        print(f"Warning: '{JSON_DB}' is empty or contains invalid JSON. Starting with an empty list.")
-        old_data = []
-    
-   
-    # append new data to the existing data
-    old_data.append(post_dict)
- 
-    # write back to the json db
-    with open(JSON_DB, 'w') as f:
-        json.dump(old_data, f, indent=3)
-
 
 @api.get('/')
 async def home():
@@ -219,12 +121,6 @@ async def update_post(id: int, upd_post: Post):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Update failed! Post with id {id} not found")  
     
-    # update the post
-    # post_dict = upd_post.model_dump()
-    # post_dict["id"] = id
-    # is_updated = update_post_func(post_dict)
-
-    # if is_updated:
 
     conn.commit()
     return {"message": "The post has been updated successfully", "data": updated_post}
